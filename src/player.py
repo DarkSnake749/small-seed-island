f8d7d5063bed56b80bd9256b9c8d644592223255
maxime <maxfortino00000gmail.com>
2024-08-21 17:15:08 -0400

    plus de configuration pour le player nouvelle classe mouvement

--- src/player.py
+++ src/player.py
@@ -3,4 +3,9 @@ class Player:
         self.color: str = "Black"
         self.__width: int = 35
         self.__height: int = 35
+        self.__pos_x: float = 0.0
+        self.__pos_y: float = 0.0
         self.__speed: int = 5
+    
+    def movement(self):
+        pass
