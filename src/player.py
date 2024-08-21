636ab2d205f9242aa2801a90872f9ed40b17a5fa
maxime <maxfortino00000gmail.com>
2024-08-21 17:24:16 -0400

    add movement and a player rect

--- src/player.py
+++ src/player.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Player:
     def __init__(self):
         self.color: str = "Black"
@@ -5,7 +7,27 @@ class Player:
         self.__height: int = 35
         self.__pos_x: float = 0.0
         self.__pos_y: float = 0.0
-        self.__speed: int = 5
+        self.player_rect = (self.__pos_x, self.__pos_y, self.__width, self.__height)
     
     def movement(self):
-        pass
+        acceleration: float = 0.5
+        vel_x: float = 0
+        vel_y: float = 0
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_w]:
+            vel_y -= 4.5
+        if key[pygame.K_s]:
+            vel_y += 4.5
+        if key[pygame.K_a]:
+            vel_x -= 4.5
+        if key[pygame.K_d]:
+            vel_x += 4.5
+
+        if key[pygame.K_w] == False and key[pygame.K_s] == False:
+            vel_y = vel_y * acceleration
+        if key[pygame.K_a] == False and key[pygame.K_d] == False:
+            vel_x = vel_x * acceleration
+        self.__pos_x += vel_x
+        self.__pos_y += vel_y
+        self.player_rect = (self.__pos_x, self.__pos_y, self.__width, self.__height)
