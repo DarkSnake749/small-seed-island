9f6b3ba8f42c5db8fc68f8c5fbe7c258b47aac49
maxime <maxfortino00000gmail.com>
2024-08-21 17:12:47 -0400

    configuration pour le player

--- src/player.py
+++ src/player.py
@@ -1,3 +1,6 @@
 class Player:
     def __init__(self):
-        pass
+        self.color: str = "Black"
+        self.__width: int = 35
+        self.__height: int = 35
+        self.__speed: int = 5
