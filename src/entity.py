0853453c414d24389e8941b84d5b21df6514a155
DarkSnake749 <etienne.boule@hotmail.com>
2024-08-22 10:48:24 -0400

    Added a debug tree in the game

--- src/entity.py
+++ src/entity.py
@@ -4,11 +4,10 @@ class Tree:
     def __init__(self, color: str =  "blue", starting_pos: tuple[int, int] = (0,0)):
         self.id: str = "tree"
         # Image
-        self.image: pygame.Surface = pygame.Surface((50, 20))
+        self.image: pygame.Surface = pygame.Surface((35, 50))
         self.image.fill(color)
         # Collider
         self.rect: pygame.Rect = self.image.get_rect(center = starting_pos)
     
     def update(self) -> None:
         pass
-    
