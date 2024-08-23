a6abd43578c2abaa72aff7698b4af3687a8bf64e
DarkSnake749 <etienne.boule@hotmail.com>
2024-08-23 12:42:21 -0400

    Added hitbox to the tree

--- src/entity.py
+++ src/entity.py
@@ -8,6 +8,8 @@ class Tree:
         self.image.fill(color)
         # Collider
         self.rect: pygame.Rect = self.image.get_rect(center = starting_pos)
+        # Hitbox
+        self.hitbox : pygame.Rect = self.rect.copy().inflate()
     
     def update(self) -> None:
         pass
