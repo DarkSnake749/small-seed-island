240d10f84f92baf0cc93d24e963668b3e166e745
lilmax69 <maxfortino00000@gmail.com>
2025-04-14 12:26:52 -0400

    ajouter une fonction pour dessiner le player

--- src/player.py
+++ src/player.py
@@ -1,6 +1,11 @@
+import pygame as pg
+
 class Player:
     def __init__(self):
         self.health: int = 100
         self.width: int = 20 #surement a modifier plus tard
         self.height: int = 20 #surement a modifier plus tard
         self.rect: tuple = (0, 0, self.width, self.height)
+
+    def draw(self, screen):
+        pg.draw.rect(screen, (255, 255, 255), self.rect)
