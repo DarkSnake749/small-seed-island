5fba04f721432e1dbe3818201fbbc2fd618db809
lilmax69 <maxfortino00000@gmail.com>
2025-04-16 12:16:28 -0400

    changer convert a convert alpha

--- src/Items.py
+++ src/Items.py
@@ -11,4 +11,4 @@ class Apple(Items):
     def __init__(self, durability):
         super().__init__(durability)
         self.durability: int = 1
-        self.surface = pg.image.load("asset/apple.png").convert()
+        self.surface = pg.image.load("asset/apple.png").convert_alpha()
